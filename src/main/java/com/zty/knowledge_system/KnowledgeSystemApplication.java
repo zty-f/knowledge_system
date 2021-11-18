9f1fbd90ac148c7b1b8851d728fd1716d4ce64ce
zty-f <2559247260@qq.com>
2021-11-18 16:51:18 +0800

    2-2 init

---
+++ src/main/java/com/zty/knowledge_system/KnowledgeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zty.knowledge_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zty/knowledge_system/KnowledgeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zty.knowledge_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
