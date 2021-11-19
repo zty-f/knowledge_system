87cd57902ba16a4bb0b67a5914f52142809edbd1
zty-f <2559247260@qq.com>
2021-11-19 19:09:50 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/zty/knowledge_system/controller/TestController.java
+++ src/main/java/com/zty/knowledge_system/controller/TestController.java
@@ -8,10 +8,14 @@
  **/
 package com.zty.knowledge_system.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}") // :TEST 设置配置默认值，配置文件没有配置，即为默认值，配置文件有配置则优先配置文件
+    private String testhello; //自定义配置文件实现
     /*@GetMapping()
     @PutMapping()
     @DeleteMapping()
@@ -21,7 +25,7 @@ public class TestController {
     @RequestMapping("/hello")
     // GET POST DELETE  PUT
     public String hello(){
-        return "Hello World!";
+        return "Hello World!"+testhello;
     }
 
     @PostMapping("/hello/post")
