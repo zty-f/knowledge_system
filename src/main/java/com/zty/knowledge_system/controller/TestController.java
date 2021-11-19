fdcd08c366c9c69dadf35d91e7cbb3a1dd3dc558
zty-f <2559247260@qq.com>
2021-11-19 10:31:55 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8080/hello

---
+++ src/main/java/com/zty/knowledge_system/controller/TestController.java
@@ -0,0 +1,26 @@
+/**
+ * @author: zty
+ * @program: knowledge_system
+ * @ClassName TestController
+ * @description:
+ * @create: 2021-11-19 10:14
+ * @Version 1.0
+ **/
+package com.zty.knowledge_system.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+    /*@GetMapping()
+    @PutMapping()
+    @DeleteMapping()
+    @PostMapping()
+    Restful风格
+    @RequestMapping(value = "/hello/1",method = RequestMethod.DELETE)*/
+    @RequestMapping("/hello")
+    // GET POST DELETE  PUT
+    public String hello(){
+        return "Hello World!";
+    }
+}
