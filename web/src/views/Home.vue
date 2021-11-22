0d51d912aec40568949a83b3edd7fa2f720e3448
zty-f <2559247260@qq.com>
2021-11-22 10:14:48 +0800

    5-5 修改axios的baseUrl读取配置文件的内容

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,8 +98,8 @@ export default defineComponent({
     const ebooks1 = reactive({books:[]});
 
     onMounted(()=>{
-      console.log("onMounted");
-      axios.get(process.env.VUE_APP_SERVER+"/ebook/list")
+      console.log("onMounted111");
+      axios.get("/ebook/list")
           .then((response)=> {
             const data = response.data;
             ebooks.value = data.content;
