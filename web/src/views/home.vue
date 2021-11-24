a89d726fe08e21e032cf6c1b10edee47a052e3db
zty-f <2559247260@qq.com>
2021-11-24 15:23:06 +0800

    6-6 首页显示电子书列表，使用固定分页参数，一次性查出所有电子书。

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -76,19 +76,19 @@
 import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
 import axios from 'axios';
 
-const listData: any= [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+/*页面测试假数据*/
+// const listData: any= [];
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export default defineComponent({
   name: 'Home',
@@ -99,26 +99,31 @@ export default defineComponent({
 
     onMounted(()=>{
       console.log("onMounted");
-      axios.get("/ebook/list")
+      axios.get("/ebook/list",{
+        params:{
+          page:1,
+          size:1000
+        }
+      })
           .then((response)=> {
             const data = response.data;
-            ebooks.value = data.content;
-            ebooks1.books = data.content;
+            ebooks.value = data.content.list;
+            // ebooks1.books = data.content.list;
             // console.log(response);
           })
     });
 
     return{
       ebooks,
-      ebooks2:toRef(ebooks1,"books"),
+      // ebooks2:toRef(ebooks1,"books"),
       /*分页功能
-      listData,
+      listData,*/
       pagination : {
         onChange: (page: any) => {
           console.log(page);
         },
         pageSize: 3,
-      },*/
+      },
       actions:  [
       { type: 'StarOutlined', text: '156' },
       { type: 'LikeOutlined', text: '156' },
