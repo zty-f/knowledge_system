52a31b4c6ad6d3cf8207fb454b1752afe47392c7
zty-f <2559247260@qq.com>
2021-11-28 14:36:12 +0800

    6-11 集成Validation，为列表查询增加数据效验

--- src/main/java/com/zty/wiki/controller/EbookController.java
+++ src/main/java/com/zty/wiki/controller/EbookController.java
@@ -17,6 +17,7 @@ import com.zty.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -35,7 +36,7 @@ public class EbookController {
     }
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req){
+    public CommonResp list(@Valid EbookQueryReq req){
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list= ebookService.list(req);
         resp.setContent(list);
