e97dd5b092ac841379996fa26bd842489fbc3d7e
zty-f <2559247260@qq.com>
2021-12-08 19:42:32 +0800

    8-7 集成富文本插件，“ wangeditor@4.6.3”，在文档表单中显示富文本框。

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -65,6 +65,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 
@@ -77,6 +80,7 @@ import {message, Modal} from "ant-design-vue";
 import { Tool } from '@/util/tool';
 import {useRoute} from "vue-router";
 import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
+import E from "wangeditor";
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -157,6 +161,8 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E("#content");
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -236,7 +242,6 @@ export default defineComponent({
       }
     };
 
-
     /**
      * 编辑
      */
@@ -246,9 +251,11 @@ export default defineComponent({
       // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
       treeSelectData.value = Tool.copy(level1.value);
       setDisable(treeSelectData.value, record.id);
-
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
     };
 
     /**
@@ -259,11 +266,12 @@ export default defineComponent({
       doc.value = {
         ebookId: route.query.ebookId
       };
-
       treeSelectData.value = Tool.copy(level1.value) || [];
-
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
     };
 
     /**
