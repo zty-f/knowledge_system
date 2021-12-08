dbb80e8062f0df1fe178e6444fa7212a6d9da1f6
zty-f <2559247260@qq.com>
2021-12-08 21:00:07 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -13,13 +13,16 @@
               <a-button type="primary"  @click="add()" size="large">新增</a-button>
             </a-form>
           </p>
+          <!-- v-if可以让有数据时才去进行表格渲染，不然有些属性没有用-->
           <a-table
+              v-if="level1.length>0"
               :columns="columns"
               :data-source="level1"
               :row-key="record => record.id"
               :loading="loading"
               :pagination="false"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text,record }">
                 {{record.sort}} {{text}}
@@ -136,6 +139,7 @@ export default defineComponent({
      *  }]
     **/
     const level1 = ref(); //一级文档树，children属性就是二级文档
+    level1.value = [];
     /**
      * 数据查询
      **/
