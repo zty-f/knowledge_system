7b45643903090081434da6708b17aefe0c40f9a5
zty-f <2559247260@qq.com>
2021-12-09 21:19:18 +0800

    8-12 增加文档页面，点击首页电子书可以进入文档页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,6 +4,7 @@ import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import Doc from '../views/doc.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -31,6 +32,10 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/doc',
     name: 'AdminDoc',
     component: AdminDoc
+  }, {
+    path: '/doc',
+    name: 'Doc',
+    component: Doc
   }
 ]
 
