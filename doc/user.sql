a8a71edc601692ff8ed652a1b6c801bad173b7d1
zty-f <2559247260@qq.com>
2021-12-11 11:04:03 +0800

    9-2 用户表的设计，并生成持久层代码

---
+++ doc/user.sql
@@ -0,0 +1,12 @@
+-- 用户表
+drop table if exists `user`;
+create table `user` (
+  `id` bigint not null comment 'ID',
+  `login_name` varchar(50) not null comment '登陆名',
+  `name` varchar(50) comment '昵称',
+  `password` char(32) not null comment '密码',
+  primary key (`id`),
+  unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
+
+insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', 'test123', 'e70e2222a9d67c4f2eae107533359aa4');
