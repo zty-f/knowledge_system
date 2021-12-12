41977011d8baa707cf58635728859e5eeb01047e
zty-f <2559247260@qq.com>
2021-12-12 20:55:13 +0800

    9-9 增加redis测试接口

--- src/main/java/com/zty/wiki/controller/TestController.java
+++ src/main/java/com/zty/wiki/controller/TestController.java
@@ -10,20 +10,25 @@ package com.zty.wiki.controller;
 
 import com.zty.wiki.domain.Test;
 import com.zty.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
 
+    private static final Logger LOG  = LoggerFactory.getLogger(TestController.class);
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
     @Value("${test.hello:TEST}") // :TEST 设置配置默认值，配置文件没有配置，即为默认值，配置文件有配置则优先配置文件
     private String testhello; //自定义配置文件实现
     /*@GetMapping()
@@ -47,6 +52,21 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable String key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+//    public Object get(@PathVariable Long key) {
+    public Object get(@PathVariable String key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 
 }
