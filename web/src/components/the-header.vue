cd0465f7970235edd6c0a8eccf9a303f1094c97c
zty-f <2559247260@qq.com>
2021-12-13 11:19:35 +0800

    9-10 vuex整合sessionStorage，解决网页刷新用户登录信息丢失问题。

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -52,7 +52,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
+import {computed, defineComponent, ref} from 'vue';
 import axios from "axios";
 import {message} from "ant-design-vue";
 import store from "@/store";
@@ -69,8 +69,7 @@ export default defineComponent({
       password:'test123'
     });
     // 登录后保存
-    const user = ref();
-    user.value={};
+    const user = computed(()=>store.state.user);
 
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -91,7 +90,6 @@ export default defineComponent({
         if(data.success){
           loginModalVisible.value = false;
           message.success("登录成功！");
-          user.value=data.content;
           store.commit("setUser",user.value);
         }else{
           message.error(data.message);
