f5a5b52046344209273ec8930eb25d1de5785ee6
zty-f <2559247260@qq.com>
2021-12-15 15:26:32 +0800

    9-13 增加直接路由请求拦截效验是否登录，未登录时跳转到首页。

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
 import AdminUser from '../views/admin/admin-user.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,26 +23,38 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     /*component: () => import(/!* webpackChunkName: "about" *!/ '../views/about.vue')*/
-  }, {
-    path: '/admin/ebook',
-    name: 'AdminEbook',
-    component: AdminEbook
-  }, {
-    path: '/admin/category',
-    name: 'AdminCategory',
-    component: AdminCategory
-  }, {
-    path: '/admin/doc',
-    name: 'AdminDoc',
-    component: AdminDoc
-  }, {
+  },  {
     path: '/doc',
     name: 'Doc',
     component: Doc
   }, {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook,
+    meta:{
+      loginRequire:true
+    }
+  }, {
+    path: '/admin/category',
+    name: 'AdminCategory',
+    component: AdminCategory,
+    meta:{
+      loginRequire:true
+    }
+  }, {
+    path: '/admin/doc',
+    name: 'AdminDoc',
+    component: AdminDoc,
+    meta:{
+      loginRequire:true
+    }
+  },{
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta:{
+      loginRequire:true
+    }
   }
 ]
 
@@ -48,5 +62,26 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 
 export default router
