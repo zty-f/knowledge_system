c5ee22a653a119f17e3631a5608deba3547fd1e2
zty-f <2559247260@qq.com>
2022-01-07 15:47:41 +0800

    10-6 增加定时器更新日志输出

--- src/main/java/com/zty/wiki/schedule/DocJob.java
+++ src/main/java/com/zty/wiki/schedule/DocJob.java
@@ -1,19 +1,18 @@
 package com.zty.wiki.schedule;
- import com.zty.wiki.service.DocService;
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.scheduling.annotation.Scheduled;
- import org.springframework.stereotype.Component;
 
- import javax.annotation.Resource;
- import java.text.SimpleDateFormat;
- import java.util.Date;
+import com.zty.wiki.service.DocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * springboot定时任务是单线程的！！！！！
  */
 @Component
- public class DocJob {
+public class DocJob {
 
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
     @Resource
@@ -23,11 +22,11 @@ package com.zty.wiki.schedule;
      * 每隔30秒钟执行一次电子书信息更新
      */
     @Scheduled(cron = "10/30 * * * * ?")
-    public void cron(){
-        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss SSS");
-        String dateString = formatter.format(new Date());
+    public void cron() {
+        LOG.info("更新电子书相关数据开始·····");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
-        LOG.info("每隔30秒钟执行一次电子书信息更新： {}", dateString);
+        LOG.info("更新电子书相关数据结束,总共耗时:{}毫秒", System.currentTimeMillis() - start);
     }
 
- }
+}
