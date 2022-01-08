7236ac8f5bf8772df694cfa07583cfb37cbbbf95
zty-f <2559247260@qq.com>
2022-01-08 15:36:02 +0800

    11-3 增加电子书快照表，生成持久层代码

---
+++ doc/ebook_snapshot.sql
@@ -0,0 +1,16 @@
+-- 电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot`
+(
+    `id`            bigint auto_increment not null comment 'id',
+    `ebook_id`      bigint                not null default 0 comment '电子书id',
+    `date`          date                  not null comment '快照日期',
+    `view_count`    int                   not null default 0 comment '阅读数',
+    `vote_count`    int                   not null default 0 comment '点赞数',
+    `view_increase` int                   not null default 0 comment '阅读增长',
+    `vote_increase` int                   not null default 0 comment '点赞增长',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='电子书快照表';
+
+# 增加唯一键 unique key `ebook_id_date_unique` (`ebook_id` , `date`)
