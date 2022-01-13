2b527c9efd09368266c93803daadc66237725463
zty-f <2559247260@qq.com>
2022-01-13 20:28:08 +0800

    12-2 注释掉测试的代码，不然打包会报错

--- src/test/java/com/zty/wiki/KnowledgeSystemApplicationTests.java
+++ src/test/java/com/zty/wiki/KnowledgeSystemApplicationTests.java
@@ -1,13 +1,13 @@
-package com.zty.wiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KnowledgeSystemApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.zty.wiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KnowledgeSystemApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
