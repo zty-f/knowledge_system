f4f116b474283433785173c34e3fba7a9f5aac48
zty-f <2559247260@qq.com>
2022-01-29 13:29:17 +0800

    12-7 完善关于我们界面！(融合vue和HTML静态网页）

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -2,4 +2,7 @@
   <div class="about">
     <h1>关于我们ZTY/FMJ</h1>
   </div>
+  <div class="iframe_box">
+    <iframe width="100%" height="1000px" src="static/about.html" frameborder="0" scrolling="auto" ref="iframeDom"></iframe>
+  </div>
 </template>
