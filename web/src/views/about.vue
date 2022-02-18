dbbe3b989c2e18aa297595a16f6fb3e5d3578c1b
zty-f <2559247260@qq.com>
2022-02-18 16:41:12 +0800

    12-9 关于我们页面加载缓慢提示。

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>关于我们ZTY/FMJ</h1>
+    <div>关于我们ZTY/FMJ (该页面采用静态资源，首次加载会比较慢，请耐心等待^_^~~~~)</div>
   </div>
   <div class="iframe_box">
     <iframe width="100%" height="1000px" src="static/about.html" frameborder="0" scrolling="auto" ref="iframeDom"></iframe>
